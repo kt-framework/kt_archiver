14f523d3642e0ac21b09cba551c9c6a977c638c8
babbino <kumotatsu@gmail.com>
2015-06-05 11:00:19 +0900

    コメント追加

--- src/test/java/jp/kt/archiver/TargzTest.java
+++ src/test/java/jp/kt/archiver/TargzTest.java
@@ -1,6 +1,9 @@
 package jp.kt.archiver;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import jp.kt.fileio.FileUtil;
 
 import org.junit.BeforeClass;
@@ -8,19 +11,33 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+/**
+ * tarファイル処理のテストクラス.
+ *
+ * @author tatsuya.kumon
+ */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TargzTest {
 	private static final String ORG_DIR = "src/main/java";
 	private static final String COMPRESS_FILE = "target/temp_for_test/compress.tar.gz";
 	private static final String DECOMPRESS_DIR = "target/temp_for_test/targz";
 
+	/**
+	 * テスト前に以前のファイルとディレクトリを削除.
+	 *
+	 * @throws IOException
+	 *             入出力エラーが発生した場合
+	 */
 	@BeforeClass
-	public static void setUp() throws Exception {
+	public static void setUp() throws IOException {
 		new FileUtil(COMPRESS_FILE).delete();
 		new FileUtil(DECOMPRESS_DIR).delete();
 		new FileUtil(DECOMPRESS_DIR).makeDirectory();
 	}
 
+	/**
+	 * 圧縮テスト.
+	 */
 	@Test
 	public void testCompress() {
 		try {
@@ -30,6 +47,9 @@ public class TargzTest {
 		}
 	}
 
+	/**
+	 * 解凍テスト.
+	 */
 	@Test
 	public void testDecompress() {
 		try {
